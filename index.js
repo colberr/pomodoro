e4dfa252b4c891e093229973a466f1bd88fcabe5
Colberr <30664849+Colberr@users.noreply.github.com>
2022-12-30 23:32:54 +0000

    Create basic menubar window

---
+++ index.js
@@ -0,0 +1,27 @@
+const app = require("electron")
+const { menubar } = require("menubar");
+const is_mac = process.platform === "darwin" // Check if OS is mac
+
+const mb = menubar({
+	browserWindow: {
+		width: 400,
+		height: 400,
+		alwaysOnTop: true,
+		
+		titleBarStyle: "hidden",
+		titleBarOverlay: true,
+
+		resizable: false,
+		minimizable: false
+	},
+	// preloadWindow: true,
+	// showOnAllWorkspaces: true,
+	windowPosition: "center"
+	// showDockIcon: false
+});
+
+mb.on("ready", () => {
+	console.log("Menubar app is ready.");
+
+	mb.showWindow()
+});
